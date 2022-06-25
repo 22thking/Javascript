702d615a82c2a3cab8a6f3c8c19024869318fe0a
root <you@example.com>
2022-06-25 07:07:25 +0000

    처음 저장합니다

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+const mongoose=require('mongoose');
+mongoose.connect
+('mongodb+srv://22thking:987654321@cluster0.wwuhp.mongodb.net/?retryWrites=true&w=majority'
+, {
+	useNewUrlParser: true, 
+	ignoreUndefined: true
+    
+}).then(()=>{
+    console.log('MongoDB Connected...');
+}).catch(()=>{
+    console.log(err);
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!~~안녕')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
